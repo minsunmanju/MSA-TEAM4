3fd4948152f6ad28685f5dc2388b841a162a5841
minsunmanju <157439430+minsunmanju@users.noreply.github.com>
2025-10-27 12:02:07 +0900

    Update WebConfig.java

--- studify-msa/post-service/src/main/java/com/lgcns/studify/post/config/WebConfig.java
+++ studify-msa/post-service/src/main/java/com/lgcns/studify/post/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://team4.store","https://lgcnsteam4.vercel.app") // 프론트 배포 주소
+                .allowedOrigins("https://team4.store","https://lgcnsteam4.vercel.app","https://lgcnsteam4-2qq4qpvu8-minsunmanjus-projects.vercel.app") // 프론트 배포 주소
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
