4c902a4d93ecd6ef03d90dd8b61ae37131550b5a
minsunmanju <157439430+minsunmanju@users.noreply.github.com>
2025-10-27 12:33:21 +0900

    Update WebConfig.java

--- studify-msa/user-service/src/main/java/com/lgcns/studify/user/config/WebConfig.java
+++ studify-msa/user-service/src/main/java/com/lgcns/studify/user/config/WebConfig.java
@@ -1,18 +1,18 @@
-package com.lgcns.studify.user.config;
+// package com.lgcns.studify.user.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://team4.store","https://lgcnsteam4.vercel.app","https://lgcnsteam4-2qq4qpvu8-minsunmanjus-projects.vercel.app") // 프론트 배포 주소
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("https://team4.store","https://lgcnsteam4.vercel.app","https://lgcnsteam4-2qq4qpvu8-minsunmanjus-projects.vercel.app") // 프론트 배포 주소
+//                 .allowedMethods("*")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+//     }
+// }
 
